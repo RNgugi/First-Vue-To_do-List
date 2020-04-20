67859625d5c4cfec49221d911ec8b84dfa2444dc
RNgugi <reubeenkihiu@gmail.com>
2020-04-20 05:45:56 +0300

    another commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,22 @@
 <template>
   <div id="app">
-    <Todos v-bind:todos="todos"/>
+    <Header />
+    <AddTodo />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos'
+import Header from './components/layout/Header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 export default {
   name: 'App',
   components: {
-    Todos
+    Header,
+    Todos,
+    AddTodo
 
   },
   data(){
@@ -24,14 +30,19 @@ export default {
         {
         id: 2,
         title: "Todo Two",
-        completed: true
+        completed: false
         },
         {
-        id: 2,
+        id: 3,
         title: "Todo Three",
         completed: false
         }
       ]
+    }
+  },
+  methods:{
+    deleteTodo(id){
+      this.todos = this.todos.filter(todo => todo.id !==id);
     }
   }
 }
@@ -47,6 +58,17 @@ body{
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
 }
-  
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor:pointer;
+}  
+.btn:hover{
+  background: #666;
+}
 
 </style>
